60490e78ea8fcd07057dc9e8d03b0c0b9ccfd8ae
PRAMOD-VISHNU-NAIK <pammunaik99.51solution@gmail.com>
2025-01-19 12:56:06 +0530

    Implemented Simple User Authentication.

--- blog/hashing.py
+++ blog/hashing.py
@@ -6,3 +6,7 @@ pwd_ctx = CryptContext(schemes=["bcrypt"], deprecated="auto")
 class Hash():
     def bcrypt(password: str):
         return pwd_ctx.hash(password)
+    
+    
+    def verify(hashed_password, plane_password):
+        return pwd_ctx.verify(plane_password, hashed_password)
